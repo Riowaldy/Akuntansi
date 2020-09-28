9c311c200e6d737bcd3cb351e39dce2a59643810
riowaldy <riowaldyi@gmail.com>
2020-09-28 22:57:29 +0700

    create pesanan pembelian

--- app/Http/Controllers/PesananPembelianController.php
+++ app/Http/Controllers/PesananPembelianController.php
@@ -33,12 +33,67 @@ class PesananPembelianController extends Controller
 
     public function getpesananpembelian()
     {
-        $tablepesananpembelian = DB::table('pesananpembelians')->select('pesananpembelians.id','pesananpembelians.no_transaksi','pesananpembelians.tanggal','suppliers.name','barangs.name as pesanan','pesananpembelians.quantity','pesananpembelians.nilai','pesananpembelians.invoice')
-                                                            ->join('barangs','barangs.id','pesananpembelians.pesanan')
+        $tablepesananpembelian = DB::table('pesananpembelians')->select('pesananpembelians.id','pesananpembelians.no_transaksi','pesananpembelians.tanggal','suppliers.name','bahans.name as pesanan','pesananpembelians.quantity','pesananpembelians.nilai','pesananpembelians.invoice')
+                                                            ->join('bahans','bahans.id','pesananpembelians.pesanan')
                                                             ->join('suppliers','suppliers.id','pesananpembelians.name')
                                                             ->orderBy('pesananpembelians.id','desc')->get();
         return response()->json(
             $tablepesananpembelian
         );
+    }
+
+    public function getdropnama(Request $request)
+    {
+        $namas = DB::table('suppliers')->select('id','name')
+                                    ->orderBy('id','asc')
+                                    ->get();
+        return $namas;
+    }
+
+    public function getdroppesanan(Request $request)
+    {
+        $pesanans = DB::table('bahans')->select('id','name')
+                                    ->orderBy('id','asc')
+                                    ->get();
+        return $pesanans;
+    }
+
+    public function addpesananpembelian(Request $request)
+    {
+        $this->validate($request, array(
+            'no_transaksi' => 'required|max:255',
+            'tanggal' => 'required|max:255',
+            'name' => 'required|max:255',
+            'pesanan' => 'required|max:255',
+            'quantity' => 'required|max:255',
+            'invoice' => 'required|max:255'
+        ));
+        
+        $pesanans = DB::table('bahans')->select('harga')
+                                    ->where('id',$request->pesanan)
+                                    ->first();
+        $harga = $pesanans->harga;
+        $hargaTot = $harga * $request->quantity;
+
+        $pesananpembelians = new Pesananpembelian();
+        $pesananpembelians->no_transaksi = $request->no_transaksi;
+        $pesananpembelians->tanggal = $request->tanggal;
+        $pesananpembelians->name = $request->name;
+        $pesananpembelians->pesanan = $request->pesanan;
+        $pesananpembelians->quantity = $request->quantity;
+        $pesananpembelians->nilai = $hargaTot;
+        $pesananpembelians->invoice = $request->invoice;
+        $pesananpembelians->save();
+
+        $hutangpiutangs = new Hutangpiutang();
+        $hutangpiutangs->no_transaksi = $request->no_transaksi;
+        $hutangpiutangs->jenis = 'Hutang';
+        $hutangpiutangs->tanggal = $request->tanggal;
+        $hutangpiutangs->name = $request->name;
+        $hutangpiutangs->pesanan = $request->pesanan;
+        $hutangpiutangs->quantity = $request->quantity;
+        $hutangpiutangs->nilai = $hargaTot;
+        $hutangpiutangs->invoice = $request->invoice;
+        $hutangpiutangs->save();
     }
 }
--- public/custom/js/pesananpembelian.js
+++ public/custom/js/pesananpembelian.js
@@ -45,11 +45,174 @@ var pesananpembelian = function () {
         } ).draw();
     };
 
+    var tambahPesananPembelian = function () {
+        $('#btn-simpan-tambahpesananpembelian').click(function(){
+            swal({
+                title: 'Apakah Anda Yakin?',
+                text: 'Menyimpan Data Pesanan Pembelian Ini',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#2196F3',
+                confirmButtonText: 'Ya',
+                cancelButtonText: 'Tidak',
+                closeOnConfirm: false,
+                closeOnCancel: true,
+                showLoaderOnConfirm: true
+            })
+            .then((isConfirm) => {
+                window.onkeydown = null;
+                window.onfocus = null;
+                if (isConfirm) {
+                    var addData = {
+                        no_transaksi: $('#notrans_pesananpembelian_tambah').val(),
+                        tanggal: $('#tanggal_pesananpembelian_tambah').val(),
+                        name: $('#nama_pesananpembelian_tambah').val(),
+                        pesanan: $('#pesanan_pesananpembelian_tambah').val(),
+                        quantity: $('#quantity_pesananpembelian_tambah').val(),
+                        invoice: $('#invoice_pesananpembelian_tambah').val(),
+                    };
+                    if(addData.no_transaksi == ""){
+                        $("#notrans_pesananpembelian_error_tambah").html("<strong>Data No Transaksi Kosong</strong>");
+                        $("#tanggal_pesananpembelian_error_tambah").html("");
+                        $("#nama_pesananpembelian_error_tambah").html("");
+                        $("#pesanan_pesananpembelian_error_tambah").html("");
+                        $("#quantity_pesananpembelian_error_tambah").html("");
+                        $("#invoice_pesananpembelian_error_tambah").html("");
+                    }
+                    else if(addData.tanggal == ""){
+                        $("#tanggal_pesananpembelian_error_tambah").html("<strong>Data Tanggal Kosong</strong>");
+                        $("#notrans_pesananpembelian_error_tambah").html("");
+                        $("#nama_pesananpembelian_error_tambah").html("");
+                        $("#pesanan_pesananpembelian_error_tambah").html("");
+                        $("#quantity_pesananpembelian_error_tambah").html("");
+                        $("#invoice_pesananpembelian_error_tambah").html("");
+                    }
+                    else if(addData.name == ""){
+                        $("#nama_pesananpembelian_error_tambah").html("<strong>Data Nama Kosong</strong>");
+                        $("#notrans_pesananpembelian_error_tambah").html("");
+                        $("#tanggal_pesananpembelian_error_tambah").html("");
+                        $("#pesanan_pesananpembelian_error_tambah").html("");
+                        $("#quantity_pesananpembelian_error_tambah").html("");
+                        $("#invoice_pesananpembelian_error_tambah").html("");
+                    }
+                    else if(addData.pesanan == ""){
+                        $("#pesanan_pesananpembelian_error_tambah").html("<strong>Data Pesanan Kosong</strong>");
+                        $("#notrans_pesananpembelian_error_tambah").html("");
+                        $("#tanggal_pesananpembelian_error_tambah").html("");
+                        $("#nama_pesananpembelian_error_tambah").html("");
+                        $("#quantity_pesananpembelian_error_tambah").html("");
+                        $("#invoice_pesananpembelian_error_tambah").html("");
+                    }
+                    else if(addData.quantity == ""){
+                        $("#quantity_pesananpembelian_error_tambah").html("<strong>Data Quantity Kosong</strong>");
+                        $("#notrans_pesananpembelian_error_tambah").html("");
+                        $("#tanggal_pesananpembelian_error_tambah").html("");
+                        $("#nama_pesananpembelian_error_tambah").html("");
+                        $("#pesanan_pesananpembelian_error_tambah").html("");
+                        $("#invoice_pesananpembelian_error_tambah").html("");
+                    }
+                    else if(addData.invoice == ""){
+                        $("#invoice_pesananpembelian_error_tambah").html("<strong>Data Invoice Kosong</strong>");
+                        $("#notrans_pesananpembelian_error_tambah").html("");
+                        $("#tanggal_pesananpembelian_error_tambah").html("");
+                        $("#nama_pesananpembelian_error_tambah").html("");
+                        $("#pesanan_pesananpembelian_error_tambah").html("");
+                        $("#quantity_pesananpembelian_error_tambah").html("");
+                    }
+                    else{
+                        $("#notrans_pesananpembelian_error_tambah").html("");
+                        $("#tanggal_pesananpembelian_error_tambah").html("");
+                        $("#nama_pesananpembelian_error_tambah").html("");
+                        $("#pesanan_pesananpembelian_error_tambah").html("");
+                        $("#quantity_pesananpembelian_error_tambah").html("");
+                        $("#invoice_pesananpembelian_error_tambah").html("");
+                        $.ajax({
+                            url : "/pesananpembelian/addpesananpembelian",
+                            type : "POST",
+                            data : addData,
+                            success: function(res){
+                                console.log(res);
+                                $('#tablepesananpembelian').DataTable().ajax.reload();
+                                    swal({
+                                        title: "Success!",
+                                        text : "Data Berhasil Ditambahkan",
+                                        confirmButtonColor: "#66BB6A",
+                                        type : "success",
+                                    });
+                                    $('#form-tambah-pesananpembelian').modal('hide');
+                                    $("#notrans_pesananpembelian_tambah").val("");
+                                    $("#tanggal_pesananpembelian_tambah").val("");
+                                    $("#nama_pesananpembelian_tambah").val("");
+                                    $("#pesanan_pesananpembelian_tambah").val("");
+                                    $("#quantity_pesananpembelian_tambah").val("");
+                                    $("#invoice_pesananpembelian_tambah").val("");
+                            },
+                            error : function(res){
+                                swal({
+                                    title: 'Error',
+                                    text : "Data Gagal Ditambahkan",
+                                    type : "error",
+                                    confirmButtonColor: "#EF5350",
+                                });
+                                $('#form-tambah-pesananpembelian').modal('hide');
+                            }
+                        })
+                    }
+                } else {
+                    swal("Aksi Dibatalkan!");
+                    $('#form-tambah-pesananpembelian').modal('hide');
+                }
+            });
+        });
+    };
+
+    var getDropNama = function(){
+        $(".nama_pesananpembelian_tambah").select2();
+        $.ajax({
+            type: 'GET',
+            url: '/pesananpembelian/getdropnama',
+            contentType: 'application/json;charset=utf-8',
+            async: false,
+            success: function (data) {
+                $('#nama_pesananpembelian_tambah').append(
+                    '<option value="" selected>-- Pilih Nama Supplier --</option>'
+                );
+                data.forEach(function(entry) {
+                    $('#nama_pesananpembelian_tambah').append(
+                        '<option value="'+ entry.id +'">'+ entry.name +'</option>'
+                    );
+                });
+            }
+        })
+    };
+
+    var getDropPesanan = function(){
+        $(".pesanan_pesananpembelian_tambah").select2();
+        $.ajax({
+            type: 'GET',
+            url: '/pesananpembelian/getdroppesanan',
+            contentType: 'application/json;charset=utf-8',
+            async: false,
+            success: function (data) {
+                $('#pesanan_pesananpembelian_tambah').append(
+                    '<option value="" selected>-- Pilih Pesanan --</option>'
+                );
+                data.forEach(function(entry) {
+                    $('#pesanan_pesananpembelian_tambah').append(
+                        '<option value="'+ entry.id +'">'+ entry.name +'</option>'
+                    );
+                });
+            }
+        })
+    };
+
     return {
         init: function () {
             getPesananPembelian();
             // resetData();
-            // tambahPesananPembelian();
+            tambahPesananPembelian();
+            getDropNama();
+            getDropPesanan();
             // getDataEditPesananPembelian();
             // editPesananPembelian();
             // deletePesananPembelian();
@@ -62,5 +225,8 @@ $(document).ready(function(){
           'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd'
+    }).datepicker("setDate", new Date());
     pesananpembelian.init();
 });
